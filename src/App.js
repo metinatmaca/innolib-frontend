7b7dbb56f9c195323d6c86cea5af28e5d0451a01
Metin Atmaca <atmacam16@itu.edu.tr>
2023-03-19 16:31:30 +0300

    initial repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import { Box } from '@mui/material'
+
+function App() {
+	return (
+		<Box
+			display={'flex'}
+			alignItems={'center'}
+			justifyContent={'center'}
+		>
+			<p>Test</p>
+		</Box>
+	)
+}
+
+export default App
